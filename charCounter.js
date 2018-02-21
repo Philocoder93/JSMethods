39d9ae69d25815b94b51787a341842a89e56434a
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2018-02-21 18:00:08 -0500

    wrote and added the charCounter method

---
+++ charCounter.js
@@ -0,0 +1,32 @@
+// this method is designed to take in a string and return
+// an object that represents how many times various characters 
+// were used in that string, it will be used to buff up various other
+// string manipulation methods
+
+function charCounter(string) {
+  return string.split('').reduce((acum, crnt) => {
+    acum[crnt] = acum[crnt] ? acum[crnt] + 1 : +!!acum[crnt] + 1;
+    return acum;
+  }, {});
+};
+
+// so this:
+// charCounter('asuh6fuerbg5uwegjwebr6iwehrg5werhgqerbfw9drugberg');
+// returns this:
+// { '5': 2,
+//   '6': 2,
+//   '9': 1,
+//   a: 1,
+//   s: 1,
+//   u: 4,
+//   h: 3,
+//   f: 2,
+//   e: 7,
+//   r: 7,
+//   b: 4,
+//   g: 6,
+//   w: 5,
+//   j: 1,
+//   i: 1,
+//   q: 1,
+//   d: 1 }
